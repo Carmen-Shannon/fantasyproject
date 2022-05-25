d1cc009a9a111045fedfc03a69fc274cb287df87
Carmen Shannon <carmen.shannon16@gmail.com>
2022-05-25 14:34:54 -0400

    Seperated into only the backend

--- backend/src/main/java/com/fantasy/backend/repositories/RosterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fantasy.backend.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.fantasy.backend.models.Roster;
-
-public interface RosterRepository extends CrudRepository<Roster,Long>{
-
-}
